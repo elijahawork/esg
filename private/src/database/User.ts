67f00a390299de68ee08e2b04a6d21b4e67f9186
Eli Simkin <es340716@g.risd.org>
2021-10-01 23:23:30 -0500

    added user concern criteria

--- private/src/database/User.ts
+++ private/src/database/User.ts
@@ -1,11 +1,17 @@
+type UserESGConcerns = {
+  environment: number;
+  social: number;
+  governance: number;
+}
 export default class User {
   username: string;
   email: string;
   password: string;
-
-  constructor(username: string, email: string, password: string) {
+  concerns: UserESGConcerns;
+  constructor(username: string, email: string, password: string, concerns: UserESGConcerns) {
     this.username = username;
     this.email = email;
     this.password = password;
+    this.concerns = concerns;
   }
 }
