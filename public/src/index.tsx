08d60ca43865f6a136f850622ef21b2b6d296976
Eli Simkin <es340716@g.risd.org>
2021-10-02 05:31:45 -0500

    Added some base xml

--- public/src/index.tsx
+++ public/src/index.tsx
@@ -1,30 +1,33 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import TTSSPeak from './TTSSPeak';
 
-type RState = {
-  timer: number;
-};
-
-class R extends React.Component<{}, RState> {
-  constructor() {
-    super({});
-    this.state = { timer: 0 };
-  }
-
-  componentDidMount() {
-    const c = setInterval(() => {
-      const incrementTimer = (prevState: RState) => ({
-        timer: prevState.timer + 1,
-      });
-      this.setState(incrementTimer);
-      if (this.state.timer > 5) {
-        clearInterval(c);
-      }
-    }, 1000);
-  }
+class App extends React.Component {
   render() {
-    return <>Time Since Init: {this.state.timer}</>;
+    return (
+      <>
+        <header>
+          <ul>
+            <li>
+              Home
+            </li>
+            <li>
+              About
+            </li>
+            <li>
+              Login
+            </li>
+            <li>Portfolio</li>
+           </ul>
+        </header>
+        <nav></nav>
+
+      </>
+    );
   }
 }
 
-ReactDOM.render(<R/>, document.body);
+ReactDOM.render(<App />, document.body);
+
+// mumble.start();
+
