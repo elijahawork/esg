2f325e3ce68a1dd7021788070d20d5fcb9aed7e2
Eli Simkin <es340716@g.risd.org>
2021-10-02 06:05:01 -0500

    moved app to its own file

---
+++ public/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export class App extends React.Component {
+  render() {
+    return (
+      <>
+        <header>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Login</li>
+            <li>Portfolio</li>
+          </ul>
+        </header>
+        <nav></nav>
+      </>
+    );
+  }
+}
--- public/src/index.tsx
+++ public/src/index.tsx
@@ -1,33 +1,5 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import TTSSPeak from './TTSSPeak';
-
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <header>
-          <ul>
-            <li>
-              Home
-            </li>
-            <li>
-              About
-            </li>
-            <li>
-              Login
-            </li>
-            <li>Portfolio</li>
-           </ul>
-        </header>
-        <nav></nav>
-
-      </>
-    );
-  }
-}
+import { App } from './APp';
 
 ReactDOM.render(<App />, document.body);
-
-// mumble.start();
-
