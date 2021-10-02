492da5e2755e33beca916ceb11414df28811b862
Eli Simkin <es340716@g.risd.org>
2021-10-01 23:36:16 -0500

    Added a sectorInterests criteria for the user

--- private/src/database/User.ts
+++ private/src/database/User.ts
@@ -2,16 +2,38 @@ type UserESGConcerns = {
   environment: number;
   social: number;
   governance: number;
+};
+enum MarketSector {
+  MATERIALS,
+  INDUSTRIALS,
+  FINANCIALS,
+  ENERGY,
+  CONSUMER_DISCRETIONARY,
+  INFORMATION_TECHNOLOGY,
+  COMMUNICATION_SERVICES,
+  REAL_ESTATE,
+  HEALTH_CARE,
+  CONSUMER_STAPLES,
+  UTILITIES,
 }
 export default class User {
   username: string;
   email: string;
   password: string;
   concerns: UserESGConcerns;
-  constructor(username: string, email: string, password: string, concerns: UserESGConcerns) {
+  sectorInterests: MarketSector[];
+  
+  constructor(
+    username: string,
+    email: string,
+    password: string,
+    concerns: UserESGConcerns,
+    sectorInterests: MarketSector[]
+  ) {
     this.username = username;
     this.email = email;
     this.password = password;
     this.concerns = concerns;
+    this.sectorInterests = sectorInterests;
   }
 }
